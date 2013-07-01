c2d0449a003aa58424eb0fdbcc89a6f8f3bdd492
Your Name Here <varunnalam@gmail.com>
2013-07-02 01:36:46 +0530

    with motion.cpp

--- motion.cpp
+++ motion.cpp
@@ -1,8 +1,12 @@
 /*
-**We shall use the direction of scoring as zero angle. And angle varies from 0 to 360
+**We shall use the direction of scoring as zero angle. And angle varies from -180 to 180
 */
 
 #include"wifi.h"
+#include "motion.h"
+#include<iostream>
+
+using namespace std;
 
 /*
 **The shoot command is to be used when there is no path for the bot to go but there is space for the ball to go.
@@ -15,10 +19,21 @@
 
 void shoot(float angle,int bot_id,int socket)  
 {
-	if(angle>45 && angle<135)
-		command = {(char)(48+bot_id),'R','~','~'};	//rotate to the right
-	else if(angle>225 && angle<315)
-		command = {(char)(48+bot_id),'L','~','~'};	//rotate to the left
+	char command[4];
+	if(angle>45 && angle<135)			//rotate to the right
+	{
+		command[0] = (char)(48+bot_id);
+		command[1] = 'R';
+		command[2] = '~';
+		command[3] = '~';	
+	}
+	else if(angle>225 && angle<315)		//rotate to the left
+	{
+		command[0] = (char)(48+bot_id);
+		command[1] = 'L';
+		command[2] = '~';
+		command[3] = '~';
+	}
 	tcpWrite(socket,command);
 }
 
@@ -31,7 +46,11 @@ void shoot(float angle,int bot_id,int socket)
 */
 void pass(int bot_id,int socket)
 {
-	command = {(char)(48+bot_id),F,'~','~'};
+	char command[4];
+	command[0] = (char)(48+bot_id);
+	command[1] = 'F';
+	command[2] = '~';
+	command[3] = '~';	
 	tcpWrite(socket,command);
 }
 
@@ -43,5 +62,114 @@ void pass(int bot_id,int socket)
 **bot_angle: Orientation of the bot
 **angle: Direction it is actually supposed to go
 **dist: Distance to destination
+**integral_error: integral error for PID
+**velocity: maximum velocity of travel
 */
-void movement(int bot_id, int socket, float bot_angle,float angle, float dist)
+void movement(int bot_id, int socket, float bot_angle,float angle, float dist, float integral_error[5]/*,int velocity */)
+{
+	int velocity=50; // shud be deleted
+	int left, right; 						//Values to the wheels
+	float error, difference, integral, derivative,time = 0.014;
+	float kp = 0.45, ki = 1.2;
+	char command[4];
+	command[0] = (char)(48+bot_id);
+
+	if(dist<10)
+	{
+			command[1] = 's';
+			command[2] = '!';
+			command[3] = '!';
+	}
+	else
+	{
+		difference = bot_angle - angle;
+	
+		if(difference<=90 && difference>=-90)
+		{
+			integral_error[bot_id] = integral_error[bot_id] + difference*time;
+			//derivative = (difference - prev_error[bot_id])/time; 
+			error = (kp*difference);// + (ki*integral_error[bot_id])+ (kd*derivative);
+
+			right = ((30 - error)*velocity/60.0);
+			left = ((error + 30)*velocity/60.0);
+		
+			if(right>velocity)
+				right = velocity;
+			if(left>velocity)
+				left = velocity;
+			
+			if(right<-velocity)
+				right =	-velocity;
+			if(left<-velocity)
+				left = -velocity;
+		
+			if(right>0 && left>0)
+			{
+				command[1] = 'F';
+				command[2] = (char)right;
+				command[3] = (char)left;
+			}
+			else if(right>0 && left<0)
+			{
+				command[1] = 'R';
+				command[2] = (char)right;
+				command[3] = (char)(-1*left);
+			}
+			else if(right<0 && left>0)
+			{
+				command[1] = 'L';
+				command[2] = (char)(-1*right);
+				command[3] = (char)(left);
+			}
+		}
+
+		else
+		{
+			if(difference>90)
+				difference = 180 - difference;
+			else if(difference<-90)
+				difference = -180-difference;
+			
+			integral_error[bot_id] = integral_error[bot_id] + difference*time;
+			//derivative = (difference - prev_error[bot_id])/time; 
+			error = (kp*difference);// + (ki*integral_error[bot_id])+ (kd*derivative);
+
+			left = ((30 - error)*velocity/60.0);
+			right = ((error + 30)*velocity/60.0);
+		
+			if(right>velocity)
+				right = velocity;
+			if(left>velocity)
+				left = velocity;
+			
+			if(right<-velocity)
+				right =	-velocity;
+			if(left<-velocity)
+				left = -velocity;
+		
+			if(right>0 && left>0)
+			{
+				command[1] = 'B';
+				command[2] = (char)right;
+				command[3] = (char)left;
+			}
+			else if(right>0 && left<0)
+			{
+				command[1] = 'L';
+				command[2] = (char)right;
+				command[3] = (char)(-1*left);
+			}
+			else if(right<0 && left>0)
+			{
+				command[1] = 'R';
+				command[2] = (char)(-1*right);
+				command[3] = (char)(left);
+			}
+		}
+
+		tcpWrite(socket,command);
+//		cout<<command<<'\n';	
+	}
+}
+			
+	
---
+++ motion.h
@@ -0,0 +1,5 @@
+#include "wifi.h"
+
+void shoot(float angle,int bot_id,int socket);
+void pass(int bot_id,int socket);
+void movement(int bot_id, int socket, float bot_angle,float angle, float dist, float integral_error[5]);
